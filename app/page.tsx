ecdfb4c1ffaa7497ca3a84ffe3a057fce5e498c4
san-geetha-s <sangeethas12122000@gmail.com>
2024-05-13 19:01:40 +0530

    added background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // pages/index.js
 'use client'
 import React, { useState } from 'react';
-
+import backgroundImage from "@/public/_8dd0afd8-2670-4280-85a1-2ce45a2d6669.jpeg"
 import SpeechBox from '@/Components/SpeechBox';
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
 
   return (
     
-      <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4" style={{ backgroundImage: `url(${backgroundImage})` }}> 
         <h1 className="text-4xl font-bold mt-8 mb-4">Text-to-Speech App</h1>
         
         <SpeechBox /> {/* Include the SpeechBox component */}
